c60a149bacf186f5dbd83302523c519ec2dd3fe1
Deric Cain <dcain@guidewire.com>
2018-08-22 09:46:51 -0500

    Fixed package.json error

--- src/modules/job/CreateModal.jsx
+++ src/modules/job/CreateModal.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Modal from 'react-modal';
 import { observer, inject } from 'mobx-react';
+import CreateJobForm from './Form';
 
 const CreateModal = ({ isOpen, job }) => (
   <Modal isOpen={isOpen}>
-  
+    <CreateJobForm />
   </Modal>
 );
 
