68104a1327f9f4d3add3afb085ddab056ee1cf74
Deric Cain <dcain@guidewire.com>
2018-08-23 16:04:44 -0500

    Added markdown editor

--- src/modules/form/markdown-group.jsx
+++ src/modules/form/markdown-group.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
+import Showdown from 'showdown';
+import Markdown from 'react-mde';
+import Preview from 'react-markdown';
 
 import { GroupWrapper, Errors, Label } from './styles';
 
 @observer
 export default class MarkdownGroup extends Component {
+  converter = new Showdown.Converter();
+
   handleOnChange = e => {
-    this.props.field.updateValue(e.currentTarget.value);
+    this.props.field.updateValue(e);
   };
 
   render() {
@@ -15,7 +20,8 @@ export default class MarkdownGroup extends Component {
       <GroupWrapper>
         <Label htmlFor={field.id}>{field.label}</Label>
         {field.hasErrors && field.isTouched ? <Errors>{field.errors[0]}</Errors> : null}
-        <textarea value={field.value} onChange={this.handleOnChange} />
+        <Markdown editorState={field.value} onChange={this.handleOnChange} />
+        <Preview>{field.value.markdown}</Preview>
       </GroupWrapper>
     );
   }
